ba5ae5b25bf9ee3d17bc22628b2805af4864fc57
Ryan Alesna <awesomman28@gmil.com>
2022-04-05 14:11:25 -0400

    added SpeechToText code, now can read in speech from a mic and convert it to text

--- main.py
+++ main.py
@@ -1,16 +1,14 @@
-# This is a sample Python script.
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+#main method
+#this is where we will call all of our functions
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+#imports
+import SpeechToText
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+
+#calling speech to text algorithm
+SpeechToText();
+
